89d6837e3d10515750807d089e8a6f8bc08035f9
Hsin-Ju Teng <dlulu900821@gmail.com>
2021-12-19 20:38:33 +0800

    資料庫建置

---
+++ database/migrations/2021_12_19_121812_create_recipes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('content')->nullable();
+            $table->string('person');
+            $table->string('time');
+            $table->string('material');
+            $table->string('step');
+            $table->string('photo_path', 2048);
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
