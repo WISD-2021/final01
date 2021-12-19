5e6cd809b0da8a986591ac4da47a45bf49a49925
Hsin-Ju Teng <dlulu900821@gmail.com>
2021-12-19 21:04:43 +0800

    新增路由

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('recipes', \App\Http\Controllers\RecipeController::class);
+Route::resource('favorites', \App\Http\Controllers\FavoriteController::class);
+Route::resource('comments', \App\Http\Controllers\CommentController::class);
+Route::resource('replies', \App\Http\Controllers\ReplyController::class);
