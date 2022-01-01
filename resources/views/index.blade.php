4040a2fe7971289e2a1c1dc324ac32e1df9cc705
chen shu ting <a.0066715@gmail.com>
2022-01-01 20:42:22 +0800

    改首頁背景圖和字體

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,14 @@
 @section('content')
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('{{ asset('img/home-bg.jpg') }}')">
+    <header class="intro-header" style="background-image: url('{{ asset('img/bg.jpg') }}')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-                        <h1>Recipes</h1>
+                        <h1><font color="#8A6BBE">Recipes</font></h1>
                         <hr class="small">
-                        <span class="subheading">Welcome</span>
+                        <span class="subheading"><font color="#8A6BBE">Welcome</font></span>
                     </div>
                 </div>
             </div>
